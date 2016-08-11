d19eb3cd8a82081a2d1e6e451cfc1b7445c73494
Prasanna <arprasanna@paypalcorp.com>
2016-08-11 09:46:01 +0530

    Updated TestMain and Rummy Hand again

--- src/RummyHand.java
+++ src/RummyHand.java
@@ -3,49 +3,42 @@ import java.util.Comparator;
 import java.util.List;
 
 public class RummyHand extends Hand {
-	
-	
-	
+
 	public RummyHand(List<Card> cards) {
 		// TODO Auto-generated constructor stub
 		this.cards = cards;
 	}
-	
+
 	public int evaluateHand(Card joker) {
 		return 0;
 	}
 
-	public static List<Card> sortedcards_suit(List<Card> list)
-	{
-	Collections.sort(list,new Comparator<Card>() {
+	public void sortedcards_suit() {
+		List<Card> list = this.cards;
+		Collections.sort(list, new Comparator<Card>() {
 
-			
 			@Override
-			public int compare(Card c1,Card c2) {
+			public int compare(Card c1, Card c2) {
 				// TODO Auto-generated method stub
-				return c2.getSuit()-c1.getSuit();
-				
+				return c2.getSuit() - c1.getSuit();
+
 			}
 
 		});
-		return list;
 	}
-	
-	public static List<Card> sortedcards_value(List<Card> list)
-	{
-	Collections.sort(list,new Comparator<Card>() {
 
-			
+	public void sortedcards_value() {
+		List<Card> list = this.cards;
+		Collections.sort(list, new Comparator<Card>() {
+
 			@Override
-			public int compare(Card c1,Card c2) {
+			public int compare(Card c1, Card c2) {
 				// TODO Auto-generated method stub
-				return c2.getFace()-c1.getFace();
-				
+				return c2.getFace() - c1.getFace();
+
 			}
 
 		});
-		return list;
 	}
-	
 
 }
--- src/TestMain.java
+++ src/TestMain.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 public class TestMain {
 
 	public static void main(String[] args) {
@@ -8,12 +6,14 @@ public class TestMain {
 		Deck deck=new Deck(2,2);
         deck.shuffle();
         RummyHand hand=new RummyHand(deck.pick(NO_OF_RUMMY_CARDS));
+        
         System.out.println(hand.cards);
-
-		List<Card> list=hand.cardsInHand();
+		
+        hand.sortedcards_value();
 		System.out.println(hand.cards);
-		System.out.println(RummyHand.sortedcards_value(list));
-		System.out.println(RummyHand.sortedcards_suit(list));
+		
+		hand.sortedcards_suit();
+		System.out.println(hand.cards);
         
         Card joker = new Card(4,1);
 		System.out.println(hand.evaluateHand(joker));

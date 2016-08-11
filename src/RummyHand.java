aca4a3c8ec33a8e4a78603f9355c694b640bd7e5
Prasanna <arprasanna@paypalcorp.com>
2016-08-11 09:57:21 +0530

    Added code for finding number of triplets

--- src/RummyHand.java
+++ src/RummyHand.java
@@ -1,5 +1,6 @@
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 public class RummyHand extends Hand {
@@ -10,7 +11,41 @@ public class RummyHand extends Hand {
 	}
 
 	public int evaluateHand(Card joker) {
+		HashMap<Integer,Integer> faceValueFrequencies = getFaceValueFrequencies();
+		int numberOfSequences;
+		int numberOfTriplets = getSets(faceValueFrequencies,3);
 		return 0;
+	}
+
+	private int getSets(HashMap<Integer, Integer> valueFrequency, int frequency) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		for(int value:valueFrequency.keySet())
+		{
+			if(valueFrequency.get(value) == frequency)
+				count++;
+		}
+		return count;
+	}
+
+	private HashMap<Integer, Integer> getFaceValueFrequencies() {
+		// TODO Auto-generated method stub
+		HashMap<Integer,Integer> faceValueFrequencies = new HashMap<Integer,Integer>();
+		
+		int rank;
+		for(int i=0;i<cards.size();i++)
+		{
+			rank = cards.get(i).getFace();
+			if(faceValueFrequencies.containsKey(rank))
+			{
+				faceValueFrequencies.put(rank, faceValueFrequencies.get(rank)+1);
+			}
+			else
+			{
+				faceValueFrequencies.put(rank, 1);
+			}
+		}
+		return faceValueFrequencies;
 	}
 
 	public void sortedcards_suit() {
